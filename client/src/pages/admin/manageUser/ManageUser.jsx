1d0c715c2897458a2d036aa23861321711c74618
JenHLee <hj.jennie.lee@gmail.com>
2022-08-03 23:36:53 -0600

    edit ManageUser.jsx after edit branch all

---
+++ client/src/pages/admin/manageUser/ManageUser.jsx
@@ -0,0 +1,32 @@
+import "./manageUser.css";
+import Users from "../../../components/users/Users";
+import axios from "axios";
+import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+
+export default function ManageUser() {
+  const [users, setUsers] = useState([]);
+  //const { search } = useLocation();
+
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const res = await axios.get("/users" + search);
+  //     setUsers(res.data);
+  //   };
+  //   fetchUsers();
+  // }, [search]);
+
+  return (
+    <>
+      <div className="home">
+        <div className="manage_user_top">
+          <h1 className="manage_user_h1">Manage Users</h1>
+          <div className="user_add_btn_container">
+            <button className="user_add_btn">Add User</button>
+          </div>
+        </div>
+        <Users users={users} />
+      </div>
+    </>
+  );
+}
