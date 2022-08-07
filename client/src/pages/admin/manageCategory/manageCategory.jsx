a3d4fcfb00beab2dd0db694012a33584a3bff731
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-08-07 14:36:18 -0600

    adds create category feature in admin page

--- client/src/pages/admin/manageCategory/manageCategory.jsx
+++ client/src/pages/admin/manageCategory/manageCategory.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useLocation } from "react-router";
+//import { useLocation } from "react-router";
 import "./manageCategory.css";
 import Categories from "../../../components/categories/Categories";
 import AddCategory from "../../../components/addCategory/AddCategory";
@@ -9,21 +9,36 @@ import AddCategory from "../../../components/addCategory/AddCategory";
 function ManageCategory() {
   const [categories, setCategories] = useState([]);
   const [addUserClicked, setAddUserClicked] = useState("false");
-  const { search } = useLocation();
 
   const addUser = () => {
     setAddUserClicked("true");
   };
 
+  const handleSubmit = async (name) => {
+    const newCategory = { name };
+
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/homeserver/api/categories",
+        newCategory
+      );
+      if (res.status === 200) {
+        alert("Category added");
+      } else {
+        alert("Category is not added");
+      }
+    } catch (err) {}
+  };
+
   useEffect(() => {
     const fetchCategories = async () => {
       const res = await axios.get(
-        "http://localhost:3000/homeserver/api/categories/" + search
+        "http://localhost:3000/homeserver/api/categories/"
       );
       setCategories(res.data);
     };
     fetchCategories();
-  }, [search]);
+  });
 
   return (
     <div className="home">
@@ -35,7 +50,9 @@ function ManageCategory() {
           </button>
         </div>
       </div>
-      {addUserClicked === "true" ? <AddCategory /> : null}
+      {addUserClicked === "true" ? (
+        <AddCategory handleSubmit={handleSubmit} />
+      ) : null}
       <Categories categories={categories} />
     </div>
   );
