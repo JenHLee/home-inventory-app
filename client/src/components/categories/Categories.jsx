427bae9d347205a08c35b68eb8be23d3b43d3c77
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-08-08 23:03:24 -0600

    fix errors when display category in Categories.jsx

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import Category from "../category/Category";
 import "./categories.css";
 
-export default function Categories({ categories }) {
+export default function Categories(props) {
+  const handleEdit = () => {
+    props.handleEdit();
+  };
+
+  const handleDelete = () => {
+    props.handleDelete();
+  };
+
   return (
     <>
       <div className="categories">
-        {categories.map((c) => (
-          <Category key={c._id} categoryprop={c} />
+        {props.categories.map((c) => (
+          <Category
+            key={c._id}
+            categoryprop={c}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
         ))}
       </div>
     </>
